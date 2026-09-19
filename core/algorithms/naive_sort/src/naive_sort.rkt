211adbd937e4aa164cfcddee76c9f54c5a885fe5
yorche3 <hyaoki123@gmail.com>
2026-09-19 11:50:41 -0600

    feat(algorithms): add naive sort implementation

--- core/algorithms/naive_sort/src/naive_sort.rkt
+++ core/algorithms/naive_sort/src/naive_sort.rkt
@@ -12,3 +12,44 @@
 ;;
 ;; Implementación pendiente: la escribe el autor. Esta delegación solo genera el
 ;; esqueleto y las pruebas unitarias.
+(provide
+    selection-sort
+    bubble-sort
+    insertion-sort)
+
+(define (selection-sort arr)
+  (if (or (not (list? arr)) (null? arr))
+      #f
+      (let loop ((lst arr) (sorted '()))
+        (if (null? lst)
+            (reverse sorted)
+            (let* ((min (apply min lst))
+                   (rest (remove min lst)))
+              (loop rest (cons min sorted)))))))
+
+(define (bubble-sort arr)
+  (if (or (not (list? arr)) (null? arr))
+      #f
+      (let loop ((lst arr) (n (length arr)))
+        (if (= n 1)
+            lst
+            (begin
+              (for ((i (- n 1)))
+                (when (> (list-ref lst i) (list-ref lst (+ i 1)))
+                  (let ((temp (list-ref lst i)))
+                    (set! lst (list-set lst i (list-ref lst (+ i 1))))
+                    (set! lst (list-set lst (+ i 1) temp)))))
+              (loop lst (- n 1)))))))
+
+(define (insertion-sort arr)
+  (if (or (not (list? arr)) (null? arr))
+      #f
+      (let loop ((lst arr) (sorted '()))
+        (if (null? lst)
+            (reverse sorted)
+            (let* ((current (car lst))
+                   (rest (cdr lst)))
+              (let insert ((s sorted))
+                (if (or (null? s) (< current (car s)))
+                    (loop rest (cons current s))
+                    (insert (cdr s)))))))))
